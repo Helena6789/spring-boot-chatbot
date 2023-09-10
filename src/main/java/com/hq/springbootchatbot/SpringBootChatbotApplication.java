1f3f3c317b273dc6ab432485fa8ba35da54f0da3
Helena <heqi899@gmail.com>
2023-09-10 14:46:06 -0400

    using local-dev properties

--- src/main/java/com/hq/springbootchatbot/SpringBootChatbotApplication.java
+++ src/main/java/com/hq/springbootchatbot/SpringBootChatbotApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootChatbotApplication {
 
     public static void main(String[] args) {
+        System.setProperty("spring.config.name", "application-dev");
         SpringApplication.run(SpringBootChatbotApplication.class, args);
     }
 
